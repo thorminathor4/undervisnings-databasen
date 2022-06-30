ae328e1be75b2176aff6341ade144a10708478ef
ThorFA <thorfa4444@gmail.com>
2022-06-30 21:29:43 +0200

    changed filenames to english dash-case small changes on posts added icon

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ import express from "express";
 import path from "path";
 
 const app = express();
-app.use(express.static("./Public"));
+app.use(express.static("./public"));
 
-app.get("/", (req, res) => res.sendFile(path.resolve("./Public/forside.html")));
-app.get("/search", (req, res) => res.sendFile(path.resolve("./Public/søgning.html")));
-app.get("/news", (req, res) => res.sendFile(path.resolve("./Public/nyheder.html")));
-app.get("/toplists", (req, res) => res.sendFile(path.resolve(`./Public/toplister.html`)));
-app.get("/toplists/list/:name", (req, res) => {
-    res.sendFile(path.resolve(`./Public/toplister/${req.params.name}.html`));
+app.get("/", (req, res) => res.sendFile(path.resolve("./public/frontpage.html")));
+app.get("/search", (req, res) => res.sendFile(path.resolve("./public/search.html")));
+app.get("/news", (req, res) => res.sendFile(path.resolve("./public/news.html")));
+app.get("/toplists", (req, res) => res.sendFile(path.resolve(`./public/toplists.html`)));
+app.get("/toplist/:name", (req, res) => {
+    res.sendFile(path.resolve(`./public/toplists/${req.params.name}.html`));
 });
-app.get("/help", (req, res) => res.sendFile(path.resolve("./Public/hjælp.html")));
-app.get("/about", (req, res) => res.sendFile(path.resolve("./Public/om.html")));
+app.get("/help", (req, res) => res.sendFile(path.resolve("./public/help.html")));
+app.get("/about", (req, res) => res.sendFile(path.resolve("./public/about.html")));
 
 app.listen(process.env.PORT || 4000);
